1d21941fe1c95bdc9362b4950a6ba7d744c93ff5
Zeyad Habash <zeyad.habashy02@gmail.com>
2026-01-07 13:05:45 +0200

    add "some" songs :3

--- Assets/Day 8/Scripts/AudioManager.cs
+++ Assets/Day 8/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource musicSource, sfxSource;
     [SerializeField] private AudioClip[] music, sfx;
 
+    private Queue<AudioClip> musicQueue;
     private void Awake()
     {
         if (Instance == null)
@@ -23,12 +25,48 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        PlayMusic("07. They Might As Well Be Dead");
+        // shuffle all songs and create a queue to play them
+        musicQueue = new Queue<AudioClip>(music);
+        ShuffleQueue(musicQueue);
+    }
+
+    private void Update()
+    {
+        if (!musicSource.isPlaying && musicQueue.Count > 0)
+        {
+            AudioClip nextClip = musicQueue.Dequeue();
+            PlayMusic(nextClip);
+            musicQueue.Enqueue(nextClip);
+        }
+    }
+
+    private void ShuffleQueue(Queue<AudioClip> queue)
+    {
+        AudioClip[] clips = queue.ToArray();
+        System.Random rng = new System.Random();
+        int n = clips.Length;
+        while (n > 1)
+        {
+            int k = rng.Next(n--);
+            AudioClip temp = clips[n];
+            clips[n] = clips[k];
+            clips[k] = temp;
+        }
+        queue.Clear();
+        foreach (var clip in clips)
+        {
+            queue.Enqueue(clip);
+        }
     }
 
     public void PlayMusic(string clipName)
     {
         AudioClip clipToPlay = Array.Find(music, x => x.name == clipName);
+        PlayMusic(clipToPlay);
+    }
+
+    public void PlayMusic(AudioClip clipToPlay)
+    {
         if (clipToPlay != null)
         {
             musicSource.clip = clipToPlay;
